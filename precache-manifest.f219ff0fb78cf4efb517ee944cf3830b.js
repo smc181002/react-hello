ef59dae6e98831e525be58ed6103682fac5004f8
smc181002 <meherchaitanya18802@gmail.com>
2020-07-05 23:54:39 +0530

    first commit

---
+++ precache-manifest.f219ff0fb78cf4efb517ee944cf3830b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e89d519a99f1852a565820e14b050888",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89f2862bbff07f26ff61",
+    "url": "/static/css/main.707ad601.chunk.css"
+  },
+  {
+    "revision": "ca2c54ea1a48d8d92196",
+    "url": "/static/js/2.6c4d4c65.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6c4d4c65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89f2862bbff07f26ff61",
+    "url": "/static/js/main.574286ac.chunk.js"
+  },
+  {
+    "revision": "402f05c59b4e2a57dec5",
+    "url": "/static/js/runtime-main.d1310a82.js"
+  },
+  {
+    "revision": "7f63a2ec82e1c0a36dfd41333406812e",
+    "url": "/static/media/Logo.7f63a2ec.svg"
+  }
+]);
